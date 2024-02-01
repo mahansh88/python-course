88e61612c3515ea495e5f966e1d93980b86de485
unknown <mahanshiri2019@gmail.com>
2024-02-01 18:26:34 +0330

    lesson string is added

---
+++ lesson2/index.py
@@ -0,0 +1,77 @@
+# x = "mahan"
+# y = 'shiri'
+# print(x == y)
+# print(x is y)
+# print(x + " " + y)
+# name = input("what is your name?")
+# print(name)
+
+# x = input("what is yor name?\t")
+# y = input("what is your last name?\t")
+# print(x + " " +y)
+##########################################
+# x = """In publishing and graphic design, 
+# Lorem ipsum is a placeholder text commonly 
+# used to demonstrate the visual form of a 
+# document or a typeface without relying on
+# meaningful content. Lorem ipsum may be used 
+# as a placeholder before the final copy is available. Wikipedia"""
+# print(isinstance(x, int))
+#######################################
+# ordered , immutable, iterable, hashable, douplicated
+
+# name = "hello mahan"
+# print(name[:9])
+
+# x = "mahan shir"
+# print(name[2:8:3])
+ 
+
+# x = "mahan ma"
+# print(x[1:3:1])
+
+x = "burden fall"
+# print(x[-7:-2])
+# if "mahan" in x:
+#     print("yes there is the name in x variable")
+# else:
+#     print("not ok, there is not name in the variable")
+############################################################
+# for i in x:
+#     print(i)
+######################################
+# hour = int(input("what time is it?\n"))
+# if hour <= 12:
+#     print("sobh bekheyr")
+# else:
+#     print("asr bekheyr")
+#############################################
+# count = 0
+# for i in range(20):
+#     if i % 2 == 1:
+#         print(i)
+###################################3
+# for i in range(0, 20, 4):
+#     print(i)
+#################################
+# name = "mahan shiri"
+# print(len(name))
+# for i in range(0, len(name), 2):
+#     print(name[i])
+#########################################
+# name = input("what is your name?\n")
+# for i in range(0, len(name), 3):
+#     print(name[i])
+##########################################
+number = int(input("what is your number?\n"))
+if number < 5:
+    print("number is less than 5")
+elif number >10:
+    print("number is more than 10")
+else:
+    print("number between 10 and 5")
+
+
+
+
+
