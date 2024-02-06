c40ac3ad9cefa38a0cecd7e408920af008fd4024
unknown <mahanshiri2019@gmail.com>
2024-02-06 15:39:14 +0330

    start list and method in list class in python

---
+++ lesson3/index.py
@@ -0,0 +1,52 @@
+# count = 0
+# # count = count + 1
+# # count += 1
+# # count = count * 1
+# # count *=1
+# # count -= 2
+# print(count)
+
+# name = "mahan"
+# print(name[-1:6:2])
+####################################3
+# a = True
+# b = False
+# print(a > b)
+#################################
+# count = 21
+# txt = f"hello my name is Mahan and my age is {count}"
+# print(txt)
+######################################
+# quantity = 3
+# itemno = 567
+# price = 49.95
+# myorder = f"I want to pay {price} dollars for {quantity} pieces of item {itemno}."
+# print(myorder)
+#######################################
+# txt = "hell my \\name is \nmahan and f\trom babol"
+# print(txt)
+########################################
+# print(dir(str))
+# txt = "hello!mah!an"
+# print(txt.split("!"))
+########################################
+# txt = "1j"
+# print(txt.capitalize())
+# print(txt.islower())
+# print(txt.isdigit())
+##############################
+# mylist = ("apple", "banana", "cherry")
+# x = "#".join(mylist)
+# print(x)
+#######################################
+# x= "thisisclassbypython"
+# print(x.count("i"))
+#####################################
+# print(x.find("Q"))
+# y= x.replace("python", "php")
+# print(x.index("Q"))
+# print(x.isalpha())
+#######################################
+# x = "    hello mahan     "
+# print(x.lstrip())
+#####################################
